03ac5067cdf7b85994f33987f2185efdb8ba1a0e
Sverda <sverda300@gmail.com>
2017-04-30 18:28:01 +0200

    Added files

---
+++ Follow_pet/Collision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Follow_pet;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Prevents flying away by punched object
+    private void OnCollisionExit(UnityEngine.Collision collision)
+    {
+        if (collision.transform == PetFollower.Target)
+        {
+            PetFollower.MoveBehaviour.StopMove();
+        }
+    }
+}
---
+++ Follow_pet/PetFollower.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Follow_pet
+{
+    public static class PetFollower
+    {
+        public static Rigidbody Rb;
+        public static Transform Target;
+        public static Transform MyTransform;
+
+        public static class MoveBehaviour
+        {
+            private static float speed = 3.0F;
+            private static Vector3 distanceLimit = new Vector3(2.0F, 2.0F, 2.0F);
+            private static Vector3 distanceFromTarget;
+
+            public static void UpdateMove()
+            {
+                if (Mathf.Abs(distanceFromTarget.x) < distanceLimit.x && Mathf.Abs(distanceFromTarget.z) < distanceLimit.z && Mathf.Abs(distanceFromTarget.y) < distanceLimit.y)
+                {
+                    // TODO: implement run in circle by 'follower'
+                    StopMove();
+                }
+                else
+                {
+                    runTo(Target);
+                }
+            }
+
+            private static void runTo(Transform t)
+            {
+                MyTransform.LookAt(t);
+                MyTransform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+
+            public static void StopMove()
+            {
+                Rb.velocity = Vector3.zero;
+                Rb.angularVelocity = Vector3.zero;
+            }
+
+            public static void ComputeDistance()
+            {
+                distanceFromTarget = Target.transform.position - MyTransform.position;
+            }
+        }
+    }
+}
