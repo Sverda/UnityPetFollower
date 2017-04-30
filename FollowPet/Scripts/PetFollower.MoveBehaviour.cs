8356c0d6c3151ac50773665ab075db28b8b4bb51
Sverda <sverda300@gmail.com>
2017-04-30 19:30:42 +0200

    Object is looking at target

---
+++ FollowPet/Scripts/PetFollower.MoveBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Follow_pet
+{
+    public static partial class PetFollower
+    {
+        public static class MoveBehaviour
+        {
+            public static float speed = 3.0F;
+            public static Vector3 distanceLimit;
+            private static Vector3 distanceFromTarget;
+
+            public static void UpdateMove()
+            {
+                if (Mathf.Abs(distanceFromTarget.x) < distanceLimit.x && Mathf.Abs(distanceFromTarget.z) < distanceLimit.z && Mathf.Abs(distanceFromTarget.y) < distanceLimit.y)
+                {
+                    // TODO: implement run in circle by 'follower'
+                    MyTransform.LookAt(Target);
+                    StopMove();
+                }
+                else
+                {
+                    runTo(Target);
+                }
+            }
+
+            private static void runTo(Transform t)
+            {
+                MyTransform.LookAt(t);
+                MyTransform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+
+            public static void StopMove()
+            {
+                Rb.velocity = Vector3.zero;
+                Rb.angularVelocity = Vector3.zero;
+            }
+
+            public static void ComputeDistance()
+            {
+                distanceFromTarget = Target.transform.position - MyTransform.position;
+            }
+        }
+    }
+}
--- FollowPet/Scripts/PetFollower.cs
+++ FollowPet/Scripts/PetFollower.cs
@@ -6,47 +6,10 @@ using UnityEngine;
 
 namespace Assets.Scripts.Follow_pet
 {
-    public static class PetFollower
+    public static partial class PetFollower
     {
         public static Rigidbody Rb;
         public static Transform Target;
         public static Transform MyTransform;
-
-        public static class MoveBehaviour
-        {
-            public static float speed = 3.0F;
-            public static Vector3 distanceLimit;
-            private static Vector3 distanceFromTarget;
-
-            public static void UpdateMove()
-            {
-                if (Mathf.Abs(distanceFromTarget.x) < distanceLimit.x && Mathf.Abs(distanceFromTarget.z) < distanceLimit.z && Mathf.Abs(distanceFromTarget.y) < distanceLimit.y)
-                {
-                    // TODO: implement run in circle by 'follower'
-                    StopMove();
-                }
-                else
-                {
-                    runTo(Target);
-                }
-            }
-
-            private static void runTo(Transform t)
-            {
-                MyTransform.LookAt(t);
-                MyTransform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
-
-            public static void StopMove()
-            {
-                Rb.velocity = Vector3.zero;
-                Rb.angularVelocity = Vector3.zero;
-            }
-
-            public static void ComputeDistance()
-            {
-                distanceFromTarget = Target.transform.position - MyTransform.position;
-            }
-        }
     }
 }
