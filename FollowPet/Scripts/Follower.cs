7ca7c8f91aa06e665192d8477bb332cde377a86b
Sverda <sverda300@gmail.com>
2017-04-30 18:51:02 +0200

    Reorganize files

---
+++ FollowPet/Scripts/Follower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Follow_pet;
+
+public class Follower : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public Vector3 distanceLimit;
+
+    private void Start()
+    {
+        PetFollower.Rb = GetComponent<Rigidbody>();
+        PetFollower.MyTransform = GetComponent<Transform>();
+        PetFollower.Target = target;
+        PetFollower.MoveBehaviour.speed = speed;
+        PetFollower.MoveBehaviour.distanceLimit = distanceLimit;
+    }
+
+    private void Update()
+    {
+        PetFollower.MoveBehaviour.UpdateMove();
+    }
+
+    private void LateUpdate()
+    {
+        PetFollower.MoveBehaviour.ComputeDistance();
+    }
+}
